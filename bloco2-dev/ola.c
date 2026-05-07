4d72acf26ed6f3f20d8c03907ddcc8df9b61ae32
sNicolas010 <nicolas.lemes@estudante.iftm.edu.br>
2026-05-07 10:35:47 -0300

    Update ola.c
    Add a simple program to print 'ola'.

--- bloco2-dev/ola.c
+++ bloco2-dev/ola.c
@@ -1,5 +1,6 @@
-/* ola.c
- * Atividade 5 — O vi
- * Escreva este programa diretamente no vi, sem colar.
- * Instruções na ficha de atividades.
- */
+#include <stdio.h>
+
+int main(){
+    printf("ola");
+    return 0;
+}
